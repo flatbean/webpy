ad521383373d66fe47c5204d947cae952fe4768f
福莱特·比恩 <46574179+flatbean@users.noreply.github.com>
2019-02-25 00:27:16 +0800

    Add files via upload

---
+++ mynewweb.py
@@ -0,0 +1,15 @@
+import web
+        
+urls = (
+    '/(.*)', 'hello'
+)
+app = web.application(urls, globals())
+
+class hello:        
+    def GET(self, name):
+        if not name: 
+            name = 'World'
+        return open(r'hello.html','r').read()
+
+if __name__ == "__main__":
+    app.run()
